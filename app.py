0c4e8adb7ed49029aa4d5539a9e8a1bdc3064835
Dattrhtn <138314408+Dattrhtn@users.noreply.github.com>
2025-06-15 18:05:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -72,7 +73,7 @@ def inde2():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    import os
+
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
