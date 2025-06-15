1aebe4bd3762ea68f3147ec72a93322bb98c711c
Dattt <dattrinh9365@gmail.com>
2025-06-15 18:17:58 +0700

    minor change 2

--- app.py
+++ app.py
@@ -19,51 +19,51 @@ def get_db_connection():
 @app.route('/predict', methods=['POST'])
 def index():
     prediction = None
-    # if request.method == 'POST':
-    #     data = request.form
+    if request.method == 'POST':
+        data = request.form
 
-    #     text_input = (
-    #         f"Tuổi: {data['age']} "
-    #         f"Giới tính: {data['gender']} "
-    #         f"Trình độ: {data['level']} "
-    #         f"Cơ sở: {data['institution']} "
-    #         f"Sinh viên CNTT: {data['isITStudent']} "
-    #         f"Vị trí: {data['position']} "
-    #         f"Tình trạng điện: {data['electricity']} "
-    #         f"Tình trạng tài chính: {data['finance']} "
-    #         f"Loại internet: {data['internetType']} "
-    #         f"Loại mạng: {data['networkType']} "
-    #         f"Thời lượng lớp: {data['classDuration']} "
-    #         f"Hệ thống tự học: {data['selfLearning']} "
-    #         f"Thiết bị: {data['device']}"
-    #     )
+        text_input = (
+            f"Tuổi: {data['age']} "
+            f"Giới tính: {data['gender']} "
+            f"Trình độ: {data['level']} "
+            f"Cơ sở: {data['institution']} "
+            f"Sinh viên CNTT: {data['isITStudent']} "
+            f"Vị trí: {data['position']} "
+            f"Tình trạng điện: {data['electricity']} "
+            f"Tình trạng tài chính: {data['finance']} "
+            f"Loại internet: {data['internetType']} "
+            f"Loại mạng: {data['networkType']} "
+            f"Thời lượng lớp: {data['classDuration']} "
+            f"Hệ thống tự học: {data['selfLearning']} "
+            f"Thiết bị: {data['device']}"
+        )
 
-    #     X_input = tfidf.transform([text_input])
-    #     y_pred = model.predict(X_input)
-    #     predicted_label = le.inverse_transform(y_pred)[0]
+        X_input = tfidf.transform([text_input])
+        y_pred = model.predict(X_input)
+        predicted_label = le.inverse_transform(y_pred)[0]
 
-    #     # Lưu vào DB
-    #     conn = get_db_connection()
-    #     cursor = conn.cursor()
-    #     sql = """
-    #         INSERT INTO predictions (
-    #             age, gender, education_level, institution_type, it_student, location,
-    #             load_shedding, financial_condition, internet_type, network_type,
-    #             class_duration, self_lms, device, predicted_level
-    #         )
-    #         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    #     """
-    #     cursor.execute(sql, (
-    #         data['age'], data['gender'], data['level'], data['institution'],
-    #         data['isITStudent'], data['position'], data['electricity'],
-    #         data['finance'], data['internetType'], data['networkType'],
-    #         data['classDuration'], data['selfLearning'], data['device'], predicted_label
-    #     ))
-    #     conn.commit()
-    #     cursor.close()
-    #     conn.close()
+        # Lưu vào DB
+        # conn = get_db_connection()
+        # cursor = conn.cursor()
+        # sql = """
+        #     INSERT INTO predictions (
+        #         age, gender, education_level, institution_type, it_student, location,
+        #         load_shedding, financial_condition, internet_type, network_type,
+        #         class_duration, self_lms, device, predicted_level
+        #     )
+        #     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        # """
+        # cursor.execute(sql, (
+        #     data['age'], data['gender'], data['level'], data['institution'],
+        #     data['isITStudent'], data['position'], data['electricity'],
+        #     data['finance'], data['internetType'], data['networkType'],
+        #     data['classDuration'], data['selfLearning'], data['device'], predicted_label
+        # ))
+        # conn.commit()
+        # cursor.close()
+        # conn.close()
 
-    #     prediction = predicted_label
+        #prediction = predicted_label
 
     return predicted_label
 
